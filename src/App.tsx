3d8138784190ce227ffaaeb8899ba65b19e567c1
oskarpicus <57806887+oskarpicus@users.noreply.github.com>
2022-11-22 19:15:11 +0200

    Task/#1 (#4)
    * Generated Swagger client for Bugsby BE
    
    * Integrated Swagger client generate in CI/CD
    
    * Added to gitignore
    
    * Implemented snackbar
    
    * Implemented BugsbyLogo component
    
    * Implemented CreateAccountPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import {Api} from "./apis/bugsbyApi";
+import BugsbySnackbar, {BugsbySnackbarProps} from "./components/BugsbySnackbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // todo retrieve from env file
+    const api = new Api({baseURL: 'http://localhost:8080'});
+    const [snackbarProps, setSnackbarProps] = useState<BugsbySnackbarProps>({
+        open: false,
+        alertProps: {}
+    });
+
+    return (
+        <BrowserRouter>
+            <div className={"App"}>
+                <BugsbySnackbar snackbarProps={snackbarProps} setSnackbarProps={setSnackbarProps}/>
+                <Routes>
+                    <Route
+                        path={"/create-account"}
+                        element={<CreateAccountPage api={api} setSnackbarProps={setSnackbarProps}/>}
+                    />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/CreateAccountPage.tsx
@@ -0,0 +1,122 @@
+import {Dispatch, FC, SetStateAction, useState} from "react";
+import {Api, UserRequest} from "../apis/bugsbyApi";
+import {BugsbySnackbarProps} from "../components/BugsbySnackbar";
+import {Box, Button, TextField, Typography} from "@mui/material";
+import BugsbyLogo from "../components/BugsbyLogo";
+import styles from "../styles/styles.module.css";
+import {Link} from "react-router-dom";
+
+interface Props {
+    api: Api<any>,
+    setSnackbarProps: Dispatch<SetStateAction<BugsbySnackbarProps>>
+}
+
+const CreateAccountPage: FC<Props> = ({api, setSnackbarProps}) => {
+    const [userRequest, setUserRequest] = useState<UserRequest>({});
+
+    const handleButtonClicked = () => api.users.createAccount(userRequest)
+        .then(response => setSnackbarProps({
+            open: true,
+            alertProps: {
+                severity: "success",
+                action: <>Created account successfully</>
+            }
+        }))
+        .catch(error => setSnackbarProps({
+            open: true,
+            alertProps: {
+                severity: "error",
+                action: <>{error.message}</>
+            }
+        }))
+
+    return (
+        <Box
+            sx={{
+                 display: "flex",
+                 flexDirection: "column",
+                 gap: "3vh"
+            }}
+            className={styles.content}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-evenly"
+                }}
+            >
+                <BugsbyLogo
+                    logoColor={"black"}
+                    textColor={"#112d4e"}
+                />
+                <Typography>
+                    Already have an account ?&nbsp;
+                    <Link
+                        to={"/"}
+                        className={styles.link}
+                    >
+                        Sign in
+                    </Link>
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignSelf: "center",
+                    gap: "15px",
+                }}
+            >
+                <Typography className={styles.title}>
+                    Create an account
+                </Typography>
+                <TextField
+                    className={styles.textField}
+                    variant={"outlined"}
+                    label={"First name"}
+                    value={userRequest.firstName ? userRequest.firstName : ""}
+                    onChange={(event) => setUserRequest(({...userRequest, firstName: event.target.value}))}
+                />
+                <TextField
+                    className={styles.textField}
+                    variant={"outlined"}
+                    label={"Last name"}
+                    value={userRequest.lastName ? userRequest.lastName : ""}
+                    onChange={(event) => setUserRequest(({...userRequest, lastName: event.target.value}))}
+                />
+                <TextField
+                    className={styles.textField}
+                    variant={"outlined"}
+                    label={"Username"}
+                    value={userRequest.username ? userRequest.username : ""}
+                    onChange={(event) => setUserRequest(({...userRequest, username: event.target.value}))}
+                />
+                <TextField
+                    className={styles.textField}
+                    variant={"outlined"}
+                    label={"Email"}
+                    value={userRequest.email ? userRequest.email : ""}
+                    onChange={(event) => setUserRequest(({...userRequest, email: event.target.value}))}
+                />
+                <TextField
+                    className={styles.textField}
+                    variant={"outlined"}
+                    type={"password"}
+                    label={"Password"}
+                    value={userRequest.password ? userRequest.password : ""}
+                    onChange={(event) => setUserRequest(({...userRequest, password: event.target.value}))}
+                />
+                <Button
+                    variant={"contained"}
+                    onClick={handleButtonClicked}
+                    className={styles.button}
+                >
+                    Create your account
+                </Button>
+            </Box>
+        </Box>
+    )
+};
+
+export default CreateAccountPage;
