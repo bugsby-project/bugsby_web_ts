8393c895ee7f3e01bbdbf6e9a93b850144b1b5fc
Oskar Picus <oskarpicus@yahoo.com>
2022-12-11 18:15:59 +0200

    Implemented AddProjectPage

---
+++ src/pages/AddProjectPage.tsx
@@ -0,0 +1,94 @@
+import {Api, AuthenticationResponse, ProjectRequest, Role} from "../apis/bugsbyApi";
+import {Dispatch, FC, SetStateAction, useEffect, useState} from "react";
+import {BugsbySnackbarProps} from "../components/BugsbySnackbar";
+import BugsbyDrawer from "../components/BugsbyDrawer";
+import {Autocomplete, Box, Button, TextField, Typography} from "@mui/material";
+import styles from "../styles/styles.module.css";
+
+interface Props {
+    api: Api<any>;
+    authenticationResponse: AuthenticationResponse;
+    setSnackbarProps: Dispatch<SetStateAction<BugsbySnackbarProps>>
+}
+
+const AddProjectPage: FC<Props> = ({api, authenticationResponse, setSnackbarProps}) => {
+    const [projectRequest, setProjectRequest] = useState<ProjectRequest>({});
+
+    useEffect(() => {
+        setProjectRequest(prevState => ({...prevState, userId: authenticationResponse.user?.id}))
+    }, [authenticationResponse.user?.id]);
+
+    const handleButtonClicked = () => {
+        if (authenticationResponse.jwt) {
+            api.projects.createProject(projectRequest)
+                .then(result => setSnackbarProps({
+                    open: true,
+                    alertProps: {
+                        severity: "success",
+                        action: <>Created project successfully</>
+                    }
+                }))
+                .catch(error => setSnackbarProps({
+                    open: true,
+                    alertProps: {
+                        severity: "error",
+                        action: <>{error.message}</>
+                    }
+                }))
+        }
+    };
+
+    const content = (
+        <Box
+            className={styles.formBox}
+        >
+            <Typography className={styles.title}>
+                Add project
+            </Typography>
+            <TextField
+                className={styles.textField}
+                variant={"outlined"}
+                label={"Title"}
+                value={projectRequest.title ? projectRequest.title : ""}
+                onChange={(event) => setProjectRequest(({...projectRequest, title: event.target.value}))}
+            />
+            <TextField
+                className={styles.textField}
+                variant={"outlined"}
+                label={"Description"}
+                value={projectRequest.description ? projectRequest.description : ""}
+                onChange={(event) => setProjectRequest(({...projectRequest, description: event.target.value}))}
+            />
+            <Autocomplete
+                disablePortal={true}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label={"Your role in the project"}
+                    />
+                )}
+                options={Object.values(Role)}
+                onChange={(event: any, newValue: string | null) =>
+                    setProjectRequest(({...projectRequest, role: newValue as Role}))}
+            />
+            <Button
+                variant={"contained"}
+                onClick={handleButtonClicked}
+                className={styles.button}
+            >
+                Save
+            </Button>
+        </Box>
+    );
+
+    return (
+        <BugsbyDrawer
+            content={content}
+            authenticationResponse={authenticationResponse}
+            contentClassName={styles.contentImplicitHeight}
+            contentStyle={{height: "100vh"}}
+        />
+    )
+};
+
+export default AddProjectPage;
